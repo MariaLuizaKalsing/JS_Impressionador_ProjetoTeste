6490bd429601087780c04395216a7d920e82d948
Maria Luiza Kalsing <luizak2010@hotmail.com>
2024-03-02 12:34:09 -0300

    Atualizadas as mensagens do sistema!

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o GitHuib");
-turboLogger("Estou aprendendo o fluxo do git!!", "black");
+turboLogger("Estou aprendendo o fluxo do git!!", "darkred");
+turboLogger(
+  "Eu consegui criar uma nova meensagem sem afetar a versão principal do sistema!",
+  "darkgreen"
+);
